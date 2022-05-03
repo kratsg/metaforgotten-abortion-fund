7e90ecc2f43fcf09dfcee73fccfd2e9f5ab972be
Giordon Stark <kratsg@gmail.com>
2022-05-03 08:39:58 -0700

    fix bug

--- js/main.js
+++ js/main.js
@@ -6,10 +6,6 @@ function SetProgress(now, ndonations)
     var percentage = now/end * 100.0;
     var distance = end - now;
 
-    if (distance < 0) {
-        document.getElementById('countdown').innerHTML = '0';
-        return;
-    }
     document.getElementById('now').innerHTML = '$' + now.toFixed(2);
     document.getElementById('percentage').innerHTML = percentage.toFixed(0) + '%';
     document.getElementById('ndonations').innerHTML = ndonations;
