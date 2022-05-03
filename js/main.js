1f3cd651d907e6c9a29f2d42af6326e566232e20
Giordon Stark <kratsg@gmail.com>
2022-05-03 08:08:17 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+SetProgress(260.25, 10)
+
+function SetProgress(now, ndonations)
+{
+    var end = 1000;
+    var percentage = now/end * 100.0;
+    var distance = end - now;
+
+    if (distance < 0) {
+        document.getElementById('countdown').innerHTML = '0';
+        return;
+    }
+    document.getElementById('now').innerHTML = now.toFixed(2);
+    document.getElementById('percentage').innerHTML = percentage.toFixed(0) + '%';
+    document.getElementById('ndonations').innerHTML = ndonations;
+}
